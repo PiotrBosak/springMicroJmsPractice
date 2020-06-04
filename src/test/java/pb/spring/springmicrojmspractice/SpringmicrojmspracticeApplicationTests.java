bba44c211913f86c20846edf5401fe0ecb26c544
piotr <PiotrBosak@yahoo.com>
2020-06-04 14:20:14 +0200

    init

---
+++ src/test/java/pb/spring/springmicrojmspractice/SpringmicrojmspracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package pb.spring.springmicrojmspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmicrojmspracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
